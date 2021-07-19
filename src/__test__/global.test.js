5eaad5564febd04b7c769479c676aed8a948a4dd
NedduJS <nmamanipantoja@gmail.com>
2021-07-19 11:36:44 -0500

    Pruebas con boolean y arrays

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,28 @@
 const text = 'Hola mundo';
+const fruits = ['manzana', 'melon', 'palta'];
 
 test('Debe contener un texto', () => {
   expect(text).toMatch(/mundo/);
+});
+
+test('¿Tenemos palta?', () => {
+  expect(fruits).toContain('palta');
+});
+
+test('Mayor que', () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test('Es verdadero', () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+  callback(str.split('').reverse().join(''));
+};
+
+test('Probar un Callback', () => {
+  reverseString('Invent', (str) => {
+    expect(str).toBe('tnevnI');
+  });
 });
